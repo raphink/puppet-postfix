77dc0a97ad406dea0e597cbb35fb9f3a07481234
Wolf Noble <wnoble@datapipe.com>
2013-07-26 18:49:21 -0500

    flesh out mailman spec

--- spec/classes/postfix_mailman_spec.rb
+++ spec/classes/postfix_mailman_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
-
 describe 'postfix::mailman' do
+  let (:facts) { {
+    :operatingsystem => 'RedHat',
+    :osfamily        => 'RedHat',
+    :fqdn            => 'fqdn.example.com',
+  } }
+	context 'when included' do
+    it { should include_class('postfix::mailman') }
+    it { should contain_postfix__config('virtual_alias_maps').with_value('hash:/etc/postfix/virtual') }
+    it { should contain_postfix__config('transport_maps').with_value('hash:/etc/postfix/transport') }
+    it { should contain_postfix__config('mailman_destination_recipient_limit').with_value('1') }
+    it { should contain_postfix__hash('/etc/postfix/virtual') }
+    it { should contain_postfix__hash('/etc/postfix/transport') }
+  end
 end
